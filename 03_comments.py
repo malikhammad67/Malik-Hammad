1c0a142d2026557f670c513708826631c86bd111
Hammad Malik <hadi1530078@gmail.com>
2026-07-21 02:43:21 -0700

    Add files via upload

---
+++ 03_comments.py
@@ -0,0 +1,40 @@
+#Python Comments
+
+##What are Comments?
+
+A comment is a note written inside the code to explain what the code does. Python ignores comments, so they are not executed.
+
+
+
+###Why Use Comments?
+
+Comments make your code easier to understand. They help you and other programmers read and maintain the code.
+
+
+#Single-Line Comment
+
+Use the # symbol to write a single-line comment.
+
+##Example:  #  this is a comment
+               
+               print("hello")
+
+##Output:hello
+
+python ignores the comment and runs the print() statement
+
+
+
+Key Points
+
+* Comments are ignored by Python.
+* Comments make code easier to read and understand.
+* Single-line comments start with #.
+* Comments do not affect the program’s output.
+
+
+Summary
+
+* Comment = A note for programmers.
+* Symbol = #
+* Python ignores comments.
