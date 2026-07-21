0ec55c30a95df59e97c9b59e4bdd1d5623010464
Hammad Malik <hadi1530078@gmail.com>
2026-07-21 02:00:32 -0700

    Add files via upload

---
+++ 02_python_syntax.py
@@ -0,0 +1,40 @@
+[12:25 AM, 7/21/2026] Malik: 
+
+Python Syntax
+
+What is Syntax?
+
+Syntax means the rules for writing Python code. If you follow the rules, your program will run. If you break the rules, Python will show an error.
+Example:
+
+
+print("hello,world")
+
+
+output:hello,world
+
+#correct vs #incorrecxt syntax
+
+#correct:  print("welcome")
+
+#Incorrect:  print("welcome)
+
+
+Reason: The closing quotation mark (") is missing.
+
+
+
+#Important Points
+
+* Python syntax is simple and easy to read.
+* Always write text inside quotes (" " or ' ').
+* Always close brackets ().
+* If the syntax is wrong, Python gives an error.
+
+
+
+#Summary
+
+* Syntax = Rules for writing Python code.
+* Follow the rules → ✅ Program runs.
+* Break the rules → ❌ Error.
