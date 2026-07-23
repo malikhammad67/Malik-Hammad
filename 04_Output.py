106c7d97babef259328a89fe5ef134b5c8a39e2d
Hammad Malik <hadi1530078@gmail.com>
2026-07-23 03:11:16 -0700

    Add files via upload

---
+++ 04_Output.py
@@ -0,0 +1,34 @@
+[📘 Python Output (print())
+
+Introduction
+
+The print() function is used to display output on the screen. It can print text, numbers, variables, and expressions.
+
+#sntax
+    print(value)
+you can print single or multile values.
+
+example_1:
+print(hello,world)
+output:
+  hello,world
+
+example_2:
+print("my age is",26)
+output
+my age is 26
+
+Key Points
+
+* print() displays output on the screen.
+* It can print text, numbers, and variables.
+* Strings must be written inside quotation marks ("" or '').
+* Multiple values can be printed using commas.
+
+⸻
+
+Summary
+
+* print() is the output function in Python.
+* It is simple, fast, and commonly used in every Python program.
+
